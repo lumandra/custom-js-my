109fef2fc0fb559451f8364913a18bbc6212d4ba
Lumandra <lumandra7@gmail.com>
2016-07-18 15:27:10 +0300

    Squares commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+function move() {
+
+  function compareRandom(a, b) {
+    return Math.random() - 0.5;
+  }
+
+  var blue = document.getElementById('square-blue');
+  var red = document.getElementById('square-red');
+  var green = document.getElementById('square-green');
+
+  var arr = [blue, green, red];
+  var sort_arr =  arr.sort(compareRandom);
+
+  for (var i = 0; i < sort_arr.length; i++){
+    console.log(sort_arr[i]);
+    document.getElementById('row').appendChild(sort_arr[i]);
+  }
+}
